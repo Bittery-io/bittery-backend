d205d58a725b86503ca9a99f07d08ec860adbf24
Przemysław Thomann <pthomann@trans.eu>
2020-08-12 22:57:50 +0200

    Copy static assets

---
+++ copy-static-assets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PWD=$(dirname "$0")
+
+cp $PWD/src/domain/services/pdf/BITTERY.jpg  $PWD/dist/domain/services/pdf/BITTERY.jpg
+cp $PWD/src/domain/services/pdf/Lato-Regular.ttf $PWD/dist/domain/services/pdf/Lato-Regular.ttf
