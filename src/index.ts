b15895a0185c0eb71808eabbfad3ae595b55474f
Przemyslaw Thomann <peerzet3@gmail.com>
2020-09-07 22:22:57 +0200

    Fix - import scheduler

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { logInfo } from './application/logging-service';
 import { getNumberProperty } from './application/property-service';
 import 'reflect-metadata';
 import { createExpressServer } from 'routing-controllers';
-
+import './domain/services/lnd-run-schedule/lnd-run-schedule-service';
 // Create a new express app instance
 export const routingControllersOptions = {
     controllers: [`${__dirname}/interfaces/**/*`],
